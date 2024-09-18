c98f06bfab373599c16cd86ad470328b905febbd
Isika Maiti <it2027@cemk.ac.in>
2024-09-18 10:16:50 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,104 @@
+
+export const movies = [
+    {
+      id: 533535,
+      title: "Deadpool & Wolverine",
+      release_date: "24-07-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+      category: "Action",
+    },
+    {
+      id: 718821,
+      title: "Twisters",
+      release_date: "10-07-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/58D6ZAvOKxlHjyX9S8qNKSBE9Y.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/pjnD08FlMAIXsfOLKQbvmO0f0MD.jpg",
+      category: "Thriller",
+    },
+    {
+      id: 1022789,
+      title: "Inside Out 2",
+      release_date: "11-06-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+      category: "Animation",
+    },
+    {
+      id: 519182,
+      title: "Despicable Me 4",
+      release_date: "20-06-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/lgkPzcOSnTvjeMnuFzozRO5HHw1.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
+      category: "Animation",
+    },
+    {
+      id: 573435,
+      title: "Bad Boys: Ride or Die",
+      release_date: "05-06-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/3q01ACG0MWm0DekhvkPFCXyPZSu.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/oGythE98MYleE6mZlGs5oBGkux1.jpg",
+      category: "Action",
+    },
+    {
+      id: 1160018,
+      title: "Kill",
+      release_date: "03-07-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/okVLmXL5y18dfN2R4ufMZEGaeCd.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/m2zXTuNPkywdYLyWlVyJZW2QOJH.jpg",
+      category: "Thriller",
+    },
+    {
+      id: 762441,
+      title: "A Quiet Place: Day One",
+      release_date: "26-06-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/2RVcJbWFmICRDsVxRI8F5xRmRsK.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/hU42CRk14JuPEdqZG3AWmagiPAP.jpg",
+      category: "Horror",
+    },
+    {
+      id: 831815,
+      title: "Saving Bikini Bottom: The Sandy Cheeks Movie",
+      release_date: "01-08-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/hdFIdXwS8FSN2wIsuotjW1mshI0.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/30YnfZdMNIV7noWLdvmcJS0cbnQ.jpg",
+      category: "Animation",
+    },
+    {
+      id: 1079091,
+      title: "It Ends with Us",
+      release_date: "07-08-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/8yPSYhooj8nyBbmV3GVdLDwuE7e.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/AjV6jFJ2YFIluYo4GQf13AA1tqu.jpg",
+      category: "Drama",
+    },
+    {
+      id: 945961,
+      title: "Alien: Romulus",
+      release_date: "13-08-2024",
+      backdrop_path:
+        "https://image.tmdb.org/t/p/original/9SSEUrSqhljBMzRe4aBTh17rUaC.jpg",
+      poster_path:
+        "https://image.tmdb.org/t/p/original/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg",
+      category: "Sci-Fi",
+    },
+  ];
+  
