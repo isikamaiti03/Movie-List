1999ac81e7d74fa8754f04f648fd15ee3d02f664
Isika Maiti <it2027@cemk.ac.in>
2024-09-20 18:26:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Movie from "./Movie";
 
+const App = () => {
+ 
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Movie/>} />
+    </Routes>
+   </> 
+  );
+};
+
+export default App;
